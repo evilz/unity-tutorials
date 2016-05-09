eab30ac8906a521096a5a9bc478919eaed8a4858
evilz <evilznet@gmail.com>
2016-05-09 21:47:06 +0200

    Roll a ball
    Roll a ball tutorial

---
+++ Roll a ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    private Vector3 _offset;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _offset = transform.position - Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = Player.transform.position + _offset;
+	}
+}
---
+++ Roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public float Speed;
+
+    private int count;
+    public Text CountText;
+    public Text WinText;
+
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Landscape;
+        _rigidbody = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        WinText.text = string.Empty;
+    }
+
+    void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        var accelerationMove = new Vector3(Input.acceleration.x, 0, -Input.acceleration.z);
+        var movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+        movement += accelerationMove;
+
+        _rigidbody.AddForce(movement * Speed);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText(); 
+        }
+    }
+
+    void SetCountText()
+    {
+        CountText.text = "Count:" + count;
+        if (count >= 12)
+        {
+            WinText.text = "You Win";
+        }
+    }
+}
