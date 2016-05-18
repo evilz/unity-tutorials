b66f67e0d9038e68f2cd01d28a0a68d1f13fedaa
evilz <evilznet@gmail.com>
2016-05-18 08:54:23 +0200

    Procedural Cave Generation - PART2
    MeshGenerator with gizmos

--- Procedural Cave Generation/Assets/MapGenerator.cs
+++ Procedural Cave Generation/Assets/MapGenerator.cs
@@ -37,6 +37,9 @@ public class MapGenerator : MonoBehaviour
         {
             SmoothMap();
         }
+
+        MeshGenerator meshGenerator = GetComponent<MeshGenerator>();
+        meshGenerator.GenerateMesh(map, 1);
     }
 
     void RandomFillMap()
@@ -107,20 +110,20 @@ public class MapGenerator : MonoBehaviour
         return wallCount;
     }
 
-    void OnDrawGizmos()
-    {
-        if (map == null) return;
+    //void OnDrawGizmos()
+    //{
+    //    if (map == null) return;
 
-        for (int x = 0; x < width; x++)
-        {
-            for (int y = 0; y < height; y++)
-            {
-                Gizmos.color = map[x, y] == 1 ? Color.black : Color.white;
-                Vector3 pos = new Vector3(-width / 2 + x + .5f, -height / 2 + y + .5f);
-                Gizmos.DrawCube(pos, Vector3.one);
-            }
-        }
-    }
+    //    for (int x = 0; x < width; x++)
+    //    {
+    //        for (int y = 0; y < height; y++)
+    //        {
+    //            Gizmos.color = map[x, y] == 1 ? Color.black : Color.white;
+    //            Vector3 pos = new Vector3(-width / 2 + x + .5f, -height / 2 + y + .5f);
+    //            Gizmos.DrawCube(pos, Vector3.one);
+    //        }
+    //    }
+    //}
 
 
 }
